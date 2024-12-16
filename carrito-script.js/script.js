7e8dc56637b00739b84aa020d16b172fe27aa6f1
proyecto-minimarket <yguaca@gmail.com>
2024-12-16 13:22:13 -0500

    se agrega el proyecto

--- carrito-script.js/script.js
+++ carrito-script.js/script.js
@@ -137,32 +137,9 @@ const showHTML = () => {
 };
 
 
-// SDK de Mercado Pago
-import { MercadoPagoConfig } from 'mercadopago';
-// Agrega credenciales
-const client = new MercadoPagoConfig({ accessToken: 'YOUR_ACCESS_TOKEN' });
-
-
-const preference = new Preference(client);
-
-preference.create({
-  body: {
-    items: [
-      {
-        title: 'Mi producto',
-        quantity: 1,
-        unit_price: 2000
-      }
-    ],
-  }
-})
-.then(console.log)
-.catch(console.log);
-
-
-const mp = new MercadoPago('YOUR_PUBLIC_KEY');
-
-const bricksBuilder = mp.bricks();
+const mercadopago = new MercadoPago("public_key", {
+	locales: "es-COP",
+});
 
 const checkoutButton = cart-product.querySelector("#checkout-btn");
 
